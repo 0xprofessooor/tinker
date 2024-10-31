2ff119117360f8482a44c4e5530c9a055a57c1d0
Rohan Tangri <rohan.tangri@gmail.com>
2024-10-31 18:41:34 +0000

    checkmark train_loop

--- tinker/dreamerV3.py
+++ tinker/dreamerV3.py
@@ -1,14 +1,22 @@
 """DreamerV3 Model-Based Deep Reinforcement Learning Algorithm."""
 
+import os
 from typing import Sequence, Tuple
 
 import chex
 import distrax
+import flashbax as fbx
 import flax.linen as nn
 import jax
+import optax
+from flashbax.buffers.trajectory_buffer import BufferState
+from flax.training.train_state import TrainState
 from gymnax.environments.environment import Environment
+from gymnax.wrappers import LogWrapper
 from jax import numpy as jnp
 from jax import scipy
+
+import wandb
 
 
 @jax.jit
@@ -133,17 +141,13 @@ class WorldModel(nn.Module):
         h: jnp.ndarray,
         z: jnp.ndarray,
     ) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
-        """Dummy method only required for parameter initialization."""
-
-        # Get latent state
+        """Dummy method for parameter initialization."""
         h = self.forward_sequence(h, z, action)
+        _ = self.forward_dynamics(h)
         z = self.forward_encoder(h, obs)
-
-        # Predict next observation, reward, and continue
         obs_pred = self.forward_decoder(h, z)
         reward_pred = self.forward_reward(h, z)
         continue_pred = self.forward_continue(h, z)
-
         return obs_pred, reward_pred, continue_pred, h, z
 
 
@@ -201,16 +205,249 @@ class ActorCriticDiscrete(nn.Module):
         return pi, jnp.squeeze(critic, axis=-1)
 
 
+@chex.dataclass(frozen=True)
+class Transition:
+    obs: jnp.ndarray
+    action: jnp.ndarray
+    reward: jnp.ndarray
+    next_obs: jnp.ndarray
+    done: jnp.ndarray
+
+
 def make_train(
     env: Environment,
     num_steps: int,
+    buffer_size: int,
+    batch_size: int,
+    world_model_config: dict,
+    actor_critic_config: dict,
+    lr: float,
+    world_model_update_freq: int,
+    uniform_mix_coeff: float = 0.01,
+    log_wandb: bool = False,
+    log_freq: int = 100,
 ):
     """Generate a jittable JAX DreamerV3 train function."""
 
+    env = LogWrapper(env)
+
     def train(rng: chex.PRNGKey):
-        # Initialize networks
-        world_model = WorldModel(env.observation_space().shape[0])
-        actor_critic = ActorCriticDiscrete(env.action_space().n)
-        params_rng, forward_rng = jax.random.split(rng)
+        # INIT DUMMY VARIABLES
+        dummy_rng = jax.random.PRNGKey(0)
+        _, dummy_state = env.reset(dummy_rng)
+        dummy_action = env.action_space().sample(dummy_rng)
+        dummy_obs, _, dummy_reward, dummy_done, _ = env.step(
+            dummy_rng, dummy_state, dummy_action
+        )
+
+        # INIT NETWORKS
+        world_model_rng, actor_critic_rng = jax.random.split(rng)
+        world_model = WorldModel(env.observation_space().shape[0], **world_model_config)
+        h = jnp.ones((1, world_model.recurrent_size))  # Initial hidden state
+        z = jnp.ones(
+            (1, world_model.stoch_size, world_model.one_hot_size)
+        )  # Initial stochastic state
+        latent_state = jnp.concatenate(
+            [h, z.reshape(-1, world_model.stoch_size * world_model.one_hot_size)],
+            axis=-1,
+        )  # latent state = flatten([h, z])
+        actor_critic = ActorCriticDiscrete(env.action_space().n, **actor_critic_config)
+
+        world_model_params = world_model.init(
+            world_model_rng, dummy_obs, dummy_action, h, z
+        )
+        actor_critic_params = actor_critic.init(actor_critic_rng, latent_state)
+
+        # INIT OPTIMIZER
+        tx = optax.adam(learning_rate=lr)
+
+        # INIT BUFFER
+        buffer = fbx.make_flat_buffer(
+            max_length=buffer_size,
+            min_length=batch_size,
+            sample_batch_size=batch_size,
+        )
+        buffer: fbx.trajectory_buffer.TrajectoryBuffer = buffer.replace(
+            init=jax.jit(buffer.init),
+            add=jax.jit(buffer.add, donate_argnums=0),
+            sample=jax.jit(buffer.sample),
+            can_sample=jax.jit(buffer.can_sample),
+        )
+        dummy_transition = Transition(
+            dummy_obs, dummy_action, dummy_reward, dummy_obs, dummy_done
+        )
+        buffer_state = buffer.init(dummy_transition)
+
+        # INIT TRAIN STATE
+        world_model_state = TrainState.create(
+            apply_fn=world_model.apply,
+            params=world_model_params,
+            tx=tx,
+        )
+        actor_critic_state = TrainState.create(
+            apply_fn=actor_critic.apply,
+            params=actor_critic_params,
+            tx=tx,
+        )
+
+        # INIT ENV
+        _, env_rng = jax.random.split(rng)
+        env_state, obs = env.reset(env_rng)
+
+        def update_world_model(
+            world_model_state: TrainState, buffer_state: BufferState, rng: chex.PRNGKey
+        ):
+            pass
+
+        def update_actor_critic():
+            pass
+
+        def train_loop(carry: tuple, env_step: int):
+            (
+                env_state,
+                obs,
+                h,
+                world_model_state,
+                actor_critic_state,
+                buffer_state,
+                rng,
+            ) = carry
+            rng_action, rng_step, world_model_update_rng, next_rng = jax.random.split(
+                rng, 4
+            )
+
+            # STEP ENV
+            z_logits = world_model.apply(
+                world_model_state.params, h, obs, method=world_model.forward_encoder
+            )
+            z_logits = _uniform_mix(z_logits, mix_coeff=uniform_mix_coeff)
+            _, rng_one_hot = jax.random.split(rng_step)
+            z = _one_hot_state(z_logits, rng_one_hot)
+            latent_state = jnp.concatenate(
+                [h, z.reshape(-1, world_model.stoch_size * world_model.one_hot_size)],
+                axis=-1,
+            )
+            pi, _ = actor_critic.apply(actor_critic_state.params, latent_state)
+            action = pi.sample(seed=rng_action)
+            next_obs, env_state, reward, done, info = env.step(
+                rng_step, env_state, action
+            )
+            h = world_model.apply(
+                world_model_state.params,
+                h,
+                z,
+                action,
+                method=world_model.forward_sequence,
+            )
+
+            # ADD TO BUFFER
+            transition = Transition(obs, action, reward, next_obs, done)
+            buffer_state = buffer.add(buffer_state, transition)
+
+            # UPDATE WORLD MODEL
+            is_world_model_update_step = (buffer.can_sample(buffer_state)) & (
+                world_model_state.step % world_model_update_freq == 0
+            )
+
+            world_model_state, total_world_loss, pred_loss, dyn_loss, rep_loss = (
+                jax.lax.cond(
+                    is_world_model_update_step,
+                    lambda world_model_state, buffer_state, world_model_update_rng: update_world_model(
+                        world_model_state, buffer_state, world_model_update_rng
+                    ),
+                    lambda world_model_state, buffer_state, world_model_update_rng: (
+                        world_model_state,
+                        jnp.array(0.0),
+                        jnp.array(0.0),
+                        jnp.array(0.0),
+                        jnp.array(0.0),
+                    ),
+                    world_model_state,
+                    buffer_state,
+                    world_model_update_rng,
+                )
+            )
+
+            # LOGGING METRICS
+            metrics = {
+                "timesteps": env_step,
+                "world_model_updates": world_model_state.step,
+                "world_model_loss": total_world_loss,
+                "pred_loss": pred_loss,
+                "dyn_loss": dyn_loss,
+                "rep_loss": rep_loss,
+                "returns": info["returned_episode_returns"],
+                "episode_lengths": info["returned_episode_lengths"],
+            }
+
+            if log_wandb:
+
+                def callback(metrics):
+                    if metrics["timesteps"] % log_freq == 0:
+                        wandb.log(metrics)
+
+                jax.debug.callback(callback, metrics)
+
+            runner_state = (
+                env_state,
+                next_obs,
+                h,
+                world_model_state,
+                actor_critic_state,
+                buffer_state,
+                next_rng,
+            )
+
+            return runner_state, metrics
+
+        runner_state = (
+            env_state,
+            obs,
+            h,
+            world_model_state,
+            actor_critic_state,
+            buffer_state,
+            rng,
+        )
+        res = jax.lax.scan(
+            train_loop,
+            init=runner_state,
+            xs=jnp.arange(1, num_steps + 1),
+            length=num_steps,
+        )
 
     return train
+
+
+if __name__ == "__main__":
+    from gymnax.environments import CartPole
+
+    env = CartPole()
+    train = make_train(
+        env=env,
+        num_steps=int(1e6),
+        num_atoms=51,
+        value_min=0,
+        value_max=500,
+        buffer_size=10000,
+        batch_size=32,
+        learning_starts=1000,
+        train_freq=10,
+        target_update_freq=500,
+        epsilon_start=1.0,
+        epsilon_end=0.01,
+        epsilon_duration=25e4,
+        lr=2.5e-4,
+        gamma=0.99,
+        tau=0.95,
+        log_wandb=True,
+    )
+    train_jit = jax.jit(train)
+
+    wandb.login(os.environ.get("WANDB_KEY"))
+    wandb.init(
+        project="Tinker",
+        tags=["DreamerV3", f"{env.name.upper()}", f"jax_{jax.__version__}"],
+        name=f"dreamer_{env.name}",
+        mode="online",
+    )
