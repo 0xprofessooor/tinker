54fc26ad3a2315e3e0cfe4ce9d214ccf11012b42
0xprofessooor <rohan.tangri@gmail.com>
2025-10-22 14:21:18 +0100

    better activation setting

--- tinker/dqn.py
+++ tinker/dqn.py
@@ -14,13 +14,14 @@ import wandb
 
 class QNetwork(nn.Module):
     action_dim: int
+    activation: callable = nn.relu
 
     @nn.compact
     def __call__(self, x: jnp.ndarray):
         x = nn.Dense(120)(x)
-        x = nn.relu(x)
+        x = self.activation(x)
         x = nn.Dense(84)(x)
-        x = nn.relu(x)
+        x = self.activation(x)
         x = nn.Dense(self.action_dim)(x)
         return x
 
