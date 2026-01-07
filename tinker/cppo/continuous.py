f7f12cd882c9bcc4b92b331efbad99fc0f8782cc
0xprofessooor <rohan.tangri@gmail.com>
2026-01-07 11:43:11 +0000

    50 battery

--- tinker/cppo/continuous.py
+++ tinker/cppo/continuous.py
@@ -468,7 +468,7 @@ if __name__ == "__main__":
     SEED = 0
     NUM_SEEDS = 5
 
-    brax_env = EcoAntV2(battery_limit=500.0)
+    brax_env = EcoAntV2(battery_limit=50.0)
     env = BraxToGymnaxWrapper(env=brax_env, episode_length=1000)
     env_params = env.default_params
 
@@ -478,15 +478,15 @@ if __name__ == "__main__":
     train_fn = make_train(
         env,
         env_params,
-        num_steps=int(2e6),
+        num_steps=int(1e6),
         train_freq=1000,
         batch_size=500,
         num_epochs=10,
         num_envs=5,
         cvar_probability=0.1,
-        cvar_limit=500.0,
-        lam_start=8.0,
-        lam_lr=1e-3,
+        cvar_limit=50.0,
+        lam_start=5.0,
+        lam_lr=1e-2,
         anneal_lr=True,
         entropy_coeff=0.0075,
     )
@@ -497,14 +497,14 @@ if __name__ == "__main__":
     print(f"Runtime: {runtime:.2f}s")
 
     log.save_local(
-        algo_name="cppo",
+        algo_name="cppo50",
         env_name=brax_env.name,
         metrics=all_metrics,
     )
 
     log.save_wandb(
         project="EcoAnt",
-        algo_name="cppo",
+        algo_name="cppo50",
         env_name=brax_env.name,
         metrics=all_metrics,
     )
